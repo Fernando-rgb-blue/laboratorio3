52f1723176c6c5acec5711850f967e4b85ebc0d7
Fernando Alfaro <fernandoalfaro209@gmail.com>
2024-06-04 23:42:59 -0500

    Laboratorio 3

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,20 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
+
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
+    <style>
+        .activo a {
+            color: red;
+            text-decoration: underline;
+        }
+    </style>
 </head>
+
 <body>
-    <ul>
-        <h1>Home</h1>
-        <li><a href="/">Home</a></li>
-        <li><a href="nosotros">Nosotros</a></li>
-        <li><a href="servicios">Servicios</a></li> <li><a href="contacto">Contacto</a></li>
-        </ul>   
+    <nav>
+        {{ dump(request()->routeIs('home')) }}
+        <table class="table">
+            @include('partials.nav')
+            @yield('content')
+        </table>
+    </nav>
 </body>
+
 </html>
 
 @yield('content')
-
